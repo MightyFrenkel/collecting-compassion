50b62b8953c417c1792431089c3b9f7eeb042dc0
Frank Hartman <frank@devhart.nl>
2022-03-29 21:15:39 +0200

    Fix auto update when new content is availaibe

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
     includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
     manifest: {
       name: 'Collecting Compassion',
